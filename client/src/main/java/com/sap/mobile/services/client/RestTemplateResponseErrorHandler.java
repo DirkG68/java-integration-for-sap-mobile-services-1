a57c292391af1147a0bd7f006cd8d2951f1c4472
Dirk Großkopf <dirk.grosskopf@sap.com>
2023-03-28 14:50:05 +0200

    Commit Work

--- client/src/main/java/com/sap/mobile/services/client/RestTemplateResponseErrorHandler.java
+++ client/src/main/java/com/sap/mobile/services/client/RestTemplateResponseErrorHandler.java
@@ -1,15 +1,28 @@
 package com.sap.mobile.services.client;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
 public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
+
+	@Getter(AccessLevel.PROTECTED)
+	private String responseBodyText;
 
 	@Override
 	public boolean hasError(ClientHttpResponse response) throws IOException {
+		try {
+		responseBodyText = IOUtils.toString(response.getBody(), StandardCharsets.UTF_8);
+		} catch (Exception e) {
+			//noop
+		}
 		return response.getStatusCode().isError();
 	}
 
@@ -20,17 +33,17 @@ public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
 
 		switch (status) {
 			case UNAUTHORIZED:
-				throw new ClientUnauthorizedException();
+				throw new ClientUnauthorizedException(responseBodyText, response.getHeaders());
 			case TOO_MANY_REQUESTS:
-				throw new TrialLimitExceededException();
+				throw new TrialLimitExceededException(responseBodyText, response.getHeaders());
 			default:
 		}
 		// Generic error handlers for undefined 4** and 5**
 		if (status.is4xxClientError()) {
-			throw new ClientErrorException(status.name());
+			throw new ClientErrorException(status.name(), responseBodyText, response.getHeaders());
 		}
 		if (status.is5xxServerError()) {
-			throw new ServerErrorException(status.name());
+			throw new ServerErrorException(status.name(), responseBodyText, response.getHeaders());
 		}
 	}
 
--- client/src/main/java/com/sap/mobile/services/client/push/PushRestTemplateResponseErrorHandler.java
+++ client/src/main/java/com/sap/mobile/services/client/push/PushRestTemplateResponseErrorHandler.java
@@ -4,17 +4,41 @@ import java.io.IOException;
 
 import org.springframework.http.client.ClientHttpResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sap.mobile.services.client.RestTemplateResponseErrorHandler;
 
 class PushRestTemplateResponseErrorHandler extends RestTemplateResponseErrorHandler {
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@Override
 	protected void handleServiceSpecificErrors(ClientHttpResponse response) throws IOException {
 		switch (response.getStatusCode()) {
+			case NOT_FOUND:
+				try {
+					PushResponse pushResponse = mapper.readValue(getResponseBodyText(), DTOPushResponse.class);
+					throw new NoMessageSentException(pushResponse.getStatus().getMessage(), getResponseBodyText(),
+							response.getHeaders(), pushResponse);
+				} catch (JsonProcessingException e) {
+					// NOOP
+				}
+				throw new NoMessageSentException(getResponseBodyText(), response.getHeaders());
 			case UNPROCESSABLE_ENTITY:
-				throw new NoMessageSentException();
-				// TODO more error handling
+				try {
+					PushResponse pushResponse = mapper.readValue(getResponseBodyText(), DTOPushResponse.class);
+					throw new MessageErrorException(getResponseBodyText(), response.getHeaders(), pushResponse);
+				} catch (JsonProcessingException e) {
+					// NOOP
+				}
+				throw new MessageErrorException(getResponseBodyText(), response.getHeaders());
 			default:
+				try {
+					PushResponse pushResponse = mapper.readValue(getResponseBodyText(), DTOPushResponse.class);
+					throw new PushClientException(response.getStatusCode().name(), getResponseBodyText(),
+							response.getHeaders(), pushResponse);
+				} catch (JsonProcessingException e) {
+					// NOOP
+				}
 		}
 	}
 }
